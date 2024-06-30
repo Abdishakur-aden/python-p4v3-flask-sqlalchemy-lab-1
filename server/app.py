a5ec4f3d997502f8c5c9d099169d4770ab5a1803
Abdishakur Mukhtar <abdishakurmukhtar46@gmail.com>
2024-06-30 18:34:30 +0300

    completed lab

--- server/app.py
+++ server/app.py
@@ -21,6 +21,39 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>', methods=['GET'])
+def earthquakes_id(id):
+    earthquakes = Earthquake.query.get(id)
+    if earthquakes:
+        earthquakes_data = {
+            'id': earthquakes.id,
+            'magnitude': earthquakes.magnitude,
+            'location': earthquakes.location,
+            'year': earthquakes.year
+        }
+        response = make_response(earthquakes_data, 200)
+        return response
+    else:
+        response = make_response({'message': f'Earthquake {id} not found.'}, 404)
+        return response
+    
+
+@app.route('/earthquakes/magnitude/<float:magnitude>', methods=['GET'])
+def earthquakes_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    quakes = [{
+        'id': quake.id,
+        'location': quake.location,
+        'magnitude': quake.magnitude,
+        'year': quake.year
+    } for quake in earthquakes]
+    quakes_data = {
+        'count': len(quakes),
+        'quakes': quakes
+    }        
+    response = make_response(quakes_data, 200)
+    return response
+    
 
 
 if __name__ == '__main__':
